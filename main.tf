360593ea7ff26c005ea7c624140ff6d86c96e3e0
nani529 <28894924+nani529@users.noreply.github.com>
2020-05-10 18:08:59 -0400

    updating main.tf file with new subnet4

--- main.tf
+++ main.tf
@@ -50,7 +50,16 @@ resource "aws_subnet" "subnet3-public" {
     }
 	
 }
+resource "aws_subnet" "subnet4-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet3_cidr}"
+    availability_zone = "us-east-1c"
 
+    tags = {
+        Name = "${var.public_subnet3_name}"
+    }
+	
+}
 
 resource "aws_route_table" "terraform-public" {
     vpc_id = "${aws_vpc.default.id}"
